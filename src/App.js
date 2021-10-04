31fda88d7ca1850f624d04a4fbcfc322f8889708
almichaud <almichaud@live.com>
2021-10-04 17:05:42 -0500

    file upload

---
+++ src/App.js
@@ -0,0 +1,34 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        NSA Malicious File Analyzer
+      </header> 
+      <div className="center">
+        <h1>View preview and confirm</h1>
+        <input class="centerFileUpload" type="file"></input>
+        <br></br>
+        <button type="button" id="preview-button">View Preview</button>
+        <br></br>
+        <br></br>
+        <br></br>
+        <br></br>
+        <br></br>
+        <br></br>
+        <br></br>
+        <br></br>
+        <br></br>
+        <br></br>
+        <br></br>
+        <br></br>
+
+
+      </div>
+    </div>
+  );
+}
+
+export default App;
