233df3c3994fb1fad03ceff28a3886974bfd6f11
yhs346 <yhs346@my.usta.edu>
2021-12-05 22:25:00 -0600

    login,register,file upload

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template
 from routes.home import home_bp
 import util.config as config
 
@@ -6,6 +6,9 @@ import util.config as config
 app = Flask(__name__)
 
 app.register_blueprint(home_bp)
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    return render_template('upload.html')
 
 if __name__ == '__main__':
     app.run(debug=1, host="0.0.0.0", port="5000")
