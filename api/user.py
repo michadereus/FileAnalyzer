4ae946d1ace807fa4b87ab02fdd649e4131fe981
Michael DeReus <michaeldereus@aol.com>
2021-12-07 10:19:01 -0600

    touchups

---
+++ api/user.py
@@ -0,0 +1,28 @@
+import sys
+sys.path.append('../')
+from database.gateway import gateway as Gateway
+from util.accesss_control import validate_api_key
+from util.response_builder import response_builder
+from flask import Blueprint, request
+import json
+
+user_bp = Blueprint('user_bp', __name__)
+
+
+# GET returns all users in db
+@user_bp.route('api/user/', methods=['GET'])
+def get_users():
+    gate = gateway()
+    if request.method == 'GET':     
+        res,code = gate.get_users()
+        # the response_builder function expects the res to be in json form
+        return response_builder(res,code)
+
+#GET returns specific user with user_id of user_id
+@user_bp.route('/user/<user_id>', methods=['GET'])
+def get_user(user_id):
+    gate = gateway()
+    if request.method == 'GET':     
+        res,code = gate.get_user(user_id)
+        # the response_builder function expects the res to be in json form
+        return response_builder(res,code)
--- app.py
+++ app.py
@@ -12,4 +12,6 @@ def index():
 if __name__ == '__main__':
     app.run(debug=1, host="0.0.0.0", port="5000")
 
+    
+
 # docker run --rm -p 5000:5000 -v $(pwd):/app server.dockerfile
