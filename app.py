e91c5ea9d1e65cbb7e60db99cfb2de1185e33579
yhs346 <yhs346@my.usta.edu>
2021-12-09 01:47:46 -0600

    fixed render

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 app.register_blueprint(home_bp)
 @app.route('/', methods=['GET', 'POST'])
 def index():
-    return render_template('upload.html')
+    return render_template('index.html')
 
 if __name__ == '__main__':
     app.run(debug=1, host="0.0.0.0", port="5000")
